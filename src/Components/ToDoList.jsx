d65fe9abcd9b513f7420f25f75aa3c08eb54e45e
mehedithedev <mehedi.developer@icloud.com>
2023-06-03 20:26:31 +0600

    added heading to my to do app

---
+++ src/Components/ToDoList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ToDoList() {
+  return (
+    <div>
+      <h1>Welcome to Mehedi's To-Do List App</h1>
+    </div>
+  )
+}
+
+export default ToDoList
+<h1>Welcome to Mehedi's To-Do List App</h1>
